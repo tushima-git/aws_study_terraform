86570481d694fce74b3812090c61307342ee5cd5
tushima <148033059+tushiko23@users.noreply.github.com>
2026-09-08 14:50:31 +0900

    outputsのsecuritygroupの説明を修正

--- modules/security_group/outputs.tf
+++ modules/security_group/outputs.tf
@@ -1,6 +1,6 @@
-# プライベートサブネットのID
+# セキュリティグループのID
 output "security_groups_ids" {
-  description = "A list of all private subnet IDs"
+  description = "A list of all security group IDs"
   value       = [
     aws_security_group.alb_sg.id,
     aws_security_group.ec2_sg.id,
